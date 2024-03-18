9022c6ea168ee7eea6803b3b58744f682ba140b1
lishal <52682738+lishal@users.noreply.github.com>
2024-03-18 22:43:47 +0545

    Created dummy product json file and created the service file

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/Services/data.service';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent implements OnInit {
-
-  constructor() { }
-
+  products: any[];
+  constructor(private dataService: DataService) {}
   ngOnInit(): void {
+    this.dataService.loadProducts().subscribe((data) => {
+      this.products = data.products;
+    });
   }
-
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './Components/carousel/carousel.component';
 import { SidebarComponent } from './Components/sidebar/sidebar.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [AppComponent, CarouselComponent, SidebarComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
