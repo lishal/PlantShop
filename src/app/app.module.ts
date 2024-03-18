8da31219a4c7e716bb0a1a53b755088197f1c2b8
lishal <52682738+lishal@users.noreply.github.com>
2024-03-18 19:10:16 +0545

    Design created for sidebar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CarouselComponent } from './Components/carousel/carousel.component';
+import { SidebarComponent } from './Components/sidebar/sidebar.component';
 
 @NgModule({
-  declarations: [AppComponent, CarouselComponent],
+  declarations: [AppComponent, CarouselComponent, SidebarComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
