7f7c26fedb49131908bbd7ecd9d3d4505da9e91b
lishal <52682738+lishal@users.noreply.github.com>
2024-03-19 02:08:04 +0545

    Logic for next and pervious button implement in slider.

--- src/app/Components/carousel/carousel.component.ts
+++ src/app/Components/carousel/carousel.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
 import { DataService } from 'src/app/Services/data.service';
 
 @Component({
@@ -8,10 +8,56 @@ import { DataService } from 'src/app/Services/data.service';
 })
 export class CarouselComponent implements OnInit {
   products: any[];
-  constructor(private dataService: DataService) {}
+  activeIndex: number = 2;
+  constructor(
+    private dataService: DataService,
+    private elementRef: ElementRef
+  ) {}
   ngOnInit(): void {
     this.dataService.loadProducts().subscribe((data) => {
       this.products = data.products;
     });
+  }
+  onPreviousBtnClick() {
+    var elementRef =
+      this.elementRef.nativeElement.children[0].getElementsByClassName(
+        'productContainer'
+      )[0];
+    var productSlider = elementRef.getElementsByClassName('productDetail');
+    for (let i = 0; i < productSlider.length; i++) {
+      productSlider[i].classList.remove('activeProduct');
+    }
+    elementRef.prepend(productSlider[productSlider.length - 1]);
+    elementRef.children[1].classList.add('activeProduct');
+    this.activeIndex =
+      elementRef.getElementsByClassName('activeProduct')[0].children[0].id;
+
+    // elementRef.children[0].classList.add('activeProduct');
+    // elementRef.productSlider[0].classList.add('activeProduct');
+    // if (this.selectedIndex === 0) {
+    //   this.selectedIndex = this.products.length - 1;
+    // } else {
+    //   this.selectedIndex--;
+    // }
+  }
+  onNextBtnClick() {
+    var elementRef =
+      this.elementRef.nativeElement.children[0].getElementsByClassName(
+        'productContainer'
+      )[0];
+    var productSlider = elementRef.getElementsByClassName('productDetail');
+    for (let i = 0; i < productSlider.length; i++) {
+      productSlider[i].classList.remove('activeProduct');
+    }
+    elementRef.append(productSlider[0]);
+    elementRef.children[1].classList.add('activeProduct');
+    this.activeIndex =
+      elementRef.getElementsByClassName('activeProduct')[0].children[0].id;
+    // elementRef.children[0].classList.add('activeProduct');
+    // if (this.selectedIndex === this.products.length - 1) {
+    //   this.selectedIndex = 0;
+    // } else {
+    //   this.selectedIndex++;
+    // }
   }
 }
